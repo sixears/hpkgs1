b5f0b53b53cb46e00d5783640dfb808722840e25
Mr. Fluffy <git@sixearsm>
2022-11-21 19:07:32 +0000

    +base1, base1t, index, tfmt

--- flake.nix
+++ flake.nix
@@ -6,26 +6,34 @@
 
     base0-src-0-0-4-11.url         = github:sixears/base0/r0.0.4.11;
     base0t-src-0-0-1-14.url        = github:sixears/base0t/r0.0.1.14;
+    base1-src-0-0-9-34.url         = github:sixears/base1/r0.0.9.34;
+    base1t-src-0-0-5-36.url        = github:sixears/base1t/r0.0.5.36;
     exited-src-1-0-4-23.url        = github:sixears/exited/r1.0.4.23;
     has-callstack-src-1-0-1-19.url = github:sixears/has-callstack/r1.0.1.19;
+    index-src-1-0-1-26.url         = github:sixears/index/r1.0.1.26;
     monaderror-io-src-1-2-5-20.url = github:sixears/monaderror-io/r1.2.5.20;
     more-unicode-src-0-0-17-12.url = github:sixears/more-unicode/r0.0.17.12;
     natural-src-0-0-1-14.url       = github:sixears/natural/r0.0.1.14;
     number-src-1-1-2-14.url        = github:sixears/number/r1.1.2.14;
     tasty-plus-src-1-5-2-24.url    = github:sixears/tasty-plus/r1.5.2.24;
+    tfmt-src-0-2-7-25.url          = github:sixears/tfmt/r0.2.7.25;
   };
 
   outputs = { self, nixpkgs, flake-utils
 
             , base0-src-0-0-4-11
             , base0t-src-0-0-1-14
+            , base1-src-0-0-9-34
+            , base1t-src-0-0-5-36
             , exited-src-1-0-4-23
             , has-callstack-src-1-0-1-19
+            , index-src-1-0-1-26
             , monaderror-io-src-1-2-5-20
             , more-unicode-src-0-0-17-12
             , natural-src-0-0-1-14
             , number-src-1-1-2-14
             , tasty-plus-src-1-5-2-24
+            , tfmt-src-0-2-7-25
             }:
     flake-utils.lib.eachDefaultSystem (system:
       let
@@ -34,6 +42,7 @@
           overlays = [ (final: prev: { haskellPackages = prev.haskell.packages.ghc8107; }) ];
         };
         hpkgs = pkgs.haskellPackages;
+        hlib  = pkgs.haskell.lib;
 
         callPkg = pname: version: src: { description, libDepends ? _: []
                                        , testDepends ? _ : []
@@ -97,6 +106,8 @@
 
           # -- L1 (internal dependencies on L0) --------------
 
+          # -- number ------------------
+
           natural          = natural-0-0;
           natural-0-0      = natural-0-0-1-14;
           natural-0-0-1-14 = callPkg "natural" "0.0.1.14" natural-src-0-0-1-14 {
@@ -105,6 +116,8 @@
           };
 
           # -- L2 (internal dependencies on L1) --------------
+
+          # -- has-callstack -----------
 
           has-callstack          = has-callstack-1-0;
           has-callstack-1-0      = has-callstack-1-0-1-19;
@@ -118,6 +131,24 @@
             };
 
           # -- L3 (internal dependencies on L2) --------------
+
+          # -- tfmt --------------------
+
+          tfmt          = tfmt-0-2;
+          tfmt-0-2      = tfmt-0-2-7-25;
+          tfmt-0-2-7-25 = callPkg "tfmt" "0.2.7.25" tfmt-src-0-2-7-25 {
+            description = "type-safe text/string formatting with a simple interface";
+            libDepends = h: with h; [
+              base containers data-textual formatting lens parsers prettyprinter
+              process tasty tasty-hunit template-haskell text text-printer time
+              trifecta
+
+              (hlib.markUnbroken text-format)
+
+              base0t has-callstack more-unicode number
+            ];
+            testDepends = h: with h; [ base tasty ];
+          };
 
           # -- monaderror-io -----------
 
@@ -165,7 +196,50 @@
             testDepends = h: with h; [ base optparse-applicative ];
           };
 
+          # -- L6 (internal dependencies on L5) --------------
 
+          # -- index -------------------
+
+          index          = index-1-0;
+          index-1-0      = index-1-0-1-26;
+          index-1-0-1-26 = callPkg "index" "1.0.1.26" index-src-1-0-1-26 {
+            description = "Class for things that are indexable";
+            libDepends = h: with h; [
+              base base-unicode-symbols hashable safe tasty tasty-hunit
+              unordered-containers
+
+              more-unicode tasty-plus
+            ];
+            testDepends = h: with h; [
+              base base-unicode-symbols optparse-applicative
+
+              more-unicode tasty-plus
+            ];
+          };
+
+          # -- L7 (internal dependencies on L6) --------------
+
+          # -- base1 -------------------
+
+          base1          = base1-0-0;
+          base1-0-0      = base1-0-0-9-34;
+          base1-0-0-9-34 = callPkg "base1" "0.0.9.34" base1-src-0-0-9-34 {
+            description = "Prelude replacement, incl. first-level local packages";
+            libDepends = h: with h; [
+              base0 has-callstack index monaderror-io more-unicode tfmt
+            ];
+          };
+
+          # -- L8 (internal dependencies on L7) --------------
+
+          # -- base1t ------------------
+
+          base1t          = base1t-0-0;
+          base1t-0-0      = base1t-0-0-5-36;
+          base1t-0-0-5-36 = callPkg "base1t" "0.0.5.36" base1t-src-0-0-5-36 {
+            description = "Prelude replacement, first-level local packages, incl. tests";
+            libDepends = h: with h; [ base0t base1 tasty-plus ];
+          };
         }; # packages = rec { ...
 
         # run, say, nix develop ~/src/hpkgs1/flake.nix#tasty-plus
