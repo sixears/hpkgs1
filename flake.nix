7364a446bc701fd8a3fc125ad87a55851da95dd1
Mr. Fluffy <git@sixearsm>
2024-08-26 17:38:21 +0000

    _

--- flake.nix
+++ flake.nix
@@ -276,11 +276,11 @@
       ref   = "r0.1.3.1";
       flake = false;
     };
-    mockio-plus-src-0-3-14-0 = {
+    mockio-plus-src-0-3-15-0 = {
       type  = "github";
       owner = "sixears";
       repo  = "mockio-plus";
-      ref   = "r0.3.14.0";
+      ref   = "r0.3.15.0";
       flake = false;
     };
     monaderror-io-src-1-2-6-0 = {
@@ -492,7 +492,7 @@
             , mockio-cmds-util-linux-src-1-0-1-3
             , mockio-src-0-0-6-0
             , mockio-log-src-0-1-3-1
-            , mockio-plus-src-0-3-14-0
+            , mockio-plus-src-0-3-15-0
             , monaderror-io-src-1-2-6-0
             , monadio-plus-src-2-5-3-0
             , more-unicode-src-0-0-19-0
@@ -1439,9 +1439,9 @@
           # -- mockio-plus -------------
 
           mockio-plus          = mockio-plus-0-3;
-          mockio-plus-0-3      = mockio-plus-0-3-14-0;
-          mockio-plus-0-3-14-0 = callPkg "mockio-plus" "0.3.14.0"
-                                         mockio-plus-src-0-3-14-0 {
+          mockio-plus-0-3      = mockio-plus-0-3-15-0;
+          mockio-plus-0-3-15-0 = callPkg "mockio-plus" "0.3.15.0"
+                                         mockio-plus-src-0-3-15-0 {
             description = "MonadIO, Mocked, Logged, with Text";
             libDepends = h: with h; [
               base bytestring containers data-default data-textual directory
